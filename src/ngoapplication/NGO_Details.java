b6c4eebcf837d186924cc38edecbaa6be0c1b275
silpa <snehaheidi831@gmail.com>
2022-03-09 18:50:28 +0530

    project

--- src/ngoapplication/NGO_Details.java
+++ src/ngoapplication/NGO_Details.java
@@ -11,5 +11,6 @@ public interface NGO_Details
     static void end()
     {
         System.out.println("Thanks for visiting us. Welcome,back.");
+        System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
     }
 }
